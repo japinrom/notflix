c404fcac320bd6efc1a836a5a5071c61799605c5
Jose Antonio <japinrom@gmail.com>
2023-01-25 17:51:52 +0100

    Add home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
+import '../../index.css'
 export default function Home () {
   return (
-    <div>Home</div>
+    <>
+      <div className='img-container' />
+      <main className='home-container'>
+        <article className='text-container'>
+          <h1>Todas las películas y series que desees, y mucho más.</h1>
+          <h2>Disfruta donde quieras. Cancela cuando quieras.</h2>
+          <Link to='/videos' className='generic-button'>Entrar</Link>
+        </article>
+      </main>
+    </>
   )
 }
